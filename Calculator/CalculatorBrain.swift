ff57ae4c902325036e2d42f44f2a81b6d0e10cd5
Sam Lee <samlee7@gmail.com>
2016-10-17 00:12:34 -0700

    added operations sin tan pow plus minus

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -17,14 +17,18 @@ class CalculatorBrain {
     }
     
     private var operations: Dictionary<String, Operation> = [
-        "π" : Operation.Constant(M_PI), //M_PI,
-        "e" : Operation.Constant(M_E),//M_E,
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
         "√" : Operation.UnaryOperation(sqrt),
         "cos" : Operation.UnaryOperation(cos),
+        "sin" : Operation.UnaryOperation(sin),
+        "tan" : Operation.UnaryOperation(tan),
+        "±" : Operation.UnaryOperation({ -$0 }),
         "×" : Operation.BinaryOperation({ $0 * $1 }),
         "÷" : Operation.BinaryOperation({ $0 / $1 }),
         "+" : Operation.BinaryOperation({ $0 + $1 }),
         "−" : Operation.BinaryOperation({ $0 - $1 }),
+        "^" : Operation.BinaryOperation(pow),
         "=" : Operation.Equals
     ]
     
